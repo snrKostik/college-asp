f39e2d50bc4615912464c115c426f958ef144313
snrKostik <kosnr@proton.me>
2026-09-15 00:57:10 +0700

    CK: added cps

--- Cookie-Clicker/ViewModels/MainViewModel.cs
+++ Cookie-Clicker/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading;
+using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Cookie_Clicker.ViewModels;
@@ -6,11 +8,45 @@ namespace Cookie_Clicker.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
 
-    private int _cookies = 00;
-    private int _perClick = 1;
+    private double _cookies = 0;
+    private double _perClick = 1;
+    private int _baseCost = 15;
+    private int _forUpgrade = 15;
+
+    private int _upgradeLevel = 0;
+
+    private double _cps = 0;
+    private int _cpsBaseCost = 15;
+    private int _cpsForUpgrade = 15;
+    private int _cpsUpgradeLevel = 0;
+
+    private readonly DispatcherTimer _timer;
 
     [ObservableProperty]
-    public partial int CookieCount {get; set;} = 0;
+    public partial double CookieCount { get; set; } = 0;
+
+    [ObservableProperty]
+    public partial double PerClick { get; set; } = 1;
+    [ObservableProperty]
+    public partial int UpgradePrice { get; set; } = 15;
+    [ObservableProperty]
+    public partial int UpgradeLevel { get; set; } = 0;
+
+    [ObservableProperty]
+    public partial double Cps { get; set; } = 0;
+    [ObservableProperty]
+    public partial double CpsUpgradePrice { get; set; } = 15;
+    [ObservableProperty]
+    public partial double CpsUpgradeLevel { get; set; } = 0;
+
+    public MainViewModel()
+    {
+        _timer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Background, (s, e) =>
+        {
+            OnTimerTick();
+        });
+        _timer.Start();
+    }
 
     public void OnCookieClick()
     {
@@ -19,11 +55,38 @@ public partial class MainViewModel : ViewModelBase
     }
     public void OnUpgradeClick()
     {
-        if (_cookies >= 50)
+        if (_cookies >= _forUpgrade)
         {
-            _perClick += 5;
-            _cookies -= 50;
+            _upgradeLevel += 1;
+            _perClick += 0.1;
+            _cookies -= _forUpgrade;
+            _forUpgrade = (int)Math.Round(_baseCost * Math.Pow(1.1, _upgradeLevel + 1));
+            UpgradePrice = _forUpgrade;
+            UpgradeLevel = _upgradeLevel;
+            PerClick = _perClick;
         }
         CookieCount = _cookies;
     }
+    public void OnUpgradeCpsClick()
+    {
+        if (_cookies >= _cpsForUpgrade)
+        {
+            _cpsUpgradeLevel += 1;
+            _cps += 0.1;
+            _cookies -= _cpsForUpgrade;
+            _cpsForUpgrade = (int)Math.Round(_cpsBaseCost * Math.Pow(1.1, _cpsUpgradeLevel + 1));
+            CpsUpgradePrice = _cpsForUpgrade;
+            CpsUpgradeLevel = _cpsUpgradeLevel;
+            Cps = _cps;
+        }
+        CookieCount = _cookies;
+    }
+
+    public void OnTimerTick() {
+        _cookies += _cps;
+        CookieCount = _cookies;
+    }
+
+    public void TimerStop() => _timer.Stop();
+
 }
--- Cookie-Clicker/Views/MainWindow.axaml.cs
+++ Cookie-Clicker/Views/MainWindow.axaml.cs
@@ -1,16 +1,34 @@
+using System;
 using System.Net;
+using System.Threading;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Threading;
 using Cookie_Clicker.ViewModels;
 
 namespace Cookie_Clicker.Views;
 
 public partial class MainWindow : Window
 {
+    private readonly MainViewModel _vm = new();
+
     public MainWindow()
     {
         InitializeComponent();
-        DataContext = new MainViewModel();
 
+        DataContext = _vm;
+        // var timer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Background, (s,e) => {
+        //
+        //         // Console.WriteLine("1 sec");
+        //
+        //
+        //         });
+        // timer.Start();
+
+    }
+    protected override void OnClosed(EventArgs e)
+    {
+        _vm.TimerStop();
+        base.OnClosed(e);
     }
 }
